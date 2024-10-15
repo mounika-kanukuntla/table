7bb11853529de0cfe6b5c608fcd2800032d69b8b
unknown <mounikakanukuntla2002@gmail.com>
2024-10-15 11:45:41 +0530

    table

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Incre from "./table.jsx"
+import "./style.css"
+let root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+ <>
+  <Incre ></Incre>
+
+ </>
+)
